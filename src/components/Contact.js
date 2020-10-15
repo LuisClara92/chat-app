355ece2f6958b861baa3fa70d6e8b98d99f8f80f
LuisClara92 <lc.sbg.92@gmail.com>
2020-10-15 17:16:51 +0100

    create contcat-list branch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,82 @@
 import React from "react";
 import "./Contact.css";
 
-export default function Contact() {
-  return (
-    <div className="Contact">
-      <img
-        className="avatar"
-        src="https://randomuser.me/api/portraits/men/99.jpg"
-        alt="Stephen Lopez"
-      />
-      <div>
-        <h3 className="name">Stephen Lopez</h3>
-        <div className="status">
-          <div className="status-online"> </div>
-          <p className="status-text"> Online</p>
-        </div>
-      </div>
+const users = [
+  {
+    name: "Robert Reyes",
+    avatar: "https://randomuser.me/api/portraits/men/28.jpg",
+    online: false
+  },
+  {
+    name: "Nellie Caldwell",
+    avatar: "https://randomuser.me/api/portraits/women/17.jpg",
+    online: true
+  },
+  {
+    name: "Vernon Mason",
+    avatar: "https://randomuser.me/api/portraits/men/84.jpg",
+    online: true
+  },
+  {
+    name: "Erica Hunt",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    online: false
+  },
+  {
+    name: "Juanita Phillips",
+    avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+    online: true
+  }
+];
+
+const Contact = (props) =>{
+  return(
+  <div className="Contact">
+  <img className="avatar" src={props.image} alt="an avatar" />
+  <div>
+    <p className="name">{props.name}</p>
+    <div className="status">
+      <div
+        className={props.status ? "status-online" : "status-offline"}
+      ></div>
+      <p className="status-text">{props.status ? "online" : "offline"}</p>
     </div>
+  </div>
+</div>
+  )}
+
+function ContactList(props){
+  return (
+    <div>
+    {users.map((item) => (
+  <Contact image={item.avatar} name={item.name} status={item.online} />
+))}</div>
+   
   );
-}
+};
+
+// export default Contact;
+export default ContactList;
+
+
+// import React from "react";
+// import "./Contact.css";
+
+// function Contact(props) {
+//   return (
+//     <div className="Contact">
+//       <img className="avatar" src={props.avatar} alt="an avatar" />
+//       <div>
+//         <p className="name">{props.name}</p>
+//         <div className="status">
+//           <div
+//             className={props.online ? "status-online" : "status-offline"}
+//           ></div>
+//           <p className="status-text">{props.online ? "online" : "offline"}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Contact;
